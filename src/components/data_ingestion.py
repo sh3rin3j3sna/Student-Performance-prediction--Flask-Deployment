20dee01fa234350e071d0cc3a6ba573188d3d743
sh3rin3j3sna <sherine.jva@gmail.com>
2023-07-12 00:43:29 +1200

    Model builder

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,8 +11,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-#from src.components.model_trainer import ModelTrainerConfig
-#from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 # any inputs regarding where to save the pre/post processed data 
@@ -62,5 +62,5 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    #modeltrainer=ModelTrainer()
-    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
